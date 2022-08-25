4fd2f247bff7642882d1fe4d4c8521f416a0e0e7
Francortiz-137 <francortiz.c@gmail.com>
2022-08-25 19:32:35 -0400

    first commit, setup of the project

---
+++ src/main/java/com/disneydemo/demo/DisneydemoApplication.java
@@ -0,0 +1,13 @@
+package com.disneydemo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneydemoApplication.class, args);
+	}
+
+}
